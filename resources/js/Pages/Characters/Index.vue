c38b6f8cbaffee779908b170f8479abfb204ea5a
vinvui <vincent.vuillemin@gmail.com>
2024-05-24 21:24:28 +0200

    Update character sheet

--- resources/js/Pages/Characters/Index.vue
+++ resources/js/Pages/Characters/Index.vue
@@ -4,19 +4,18 @@
         <Menu curent="Mes Personnages"></Menu>
 
         <v-main class="px-4 my-8 flex flex-col gap-4">
-            <v-sheet v-for="character in characters" :key="character.id" @click="viewCharacter(character.id)"
-                class="p-4" elevation="12" max-width="600" rounded="lg" width="100%">
+            <v-card v-for="character in characters" :key="character.id" @click="viewCharacter(character.id)" class="px-4 py-2 border" elevation="12" max-width="600" rounded="lg" width="100%">
                 <div class="flex items-center gap-4">
-                    <v-avatar>
+                    <v-avatar size="50">
                         <v-img :alt="character.name" :src="character.image"></v-img>
                     </v-avatar>
                     <div class="flex flex-col">
                         <div class="">{{ character.name }}</div>
-                        <div class="">{{ character.race }}</div>
-                        <div class="">{{ character.class }} niv. {{ character.level }}</div>
+                        <div class="text-xs text-gray-400">{{ character.race }}</div>
+                        <div class="text-xs text-gray-400">{{ character.class }} niv. {{ character.level }}</div>
                     </div>
                 </div>
-            </v-sheet>
+            </v-card>
         </v-main>
     </v-layout>
 </template>
